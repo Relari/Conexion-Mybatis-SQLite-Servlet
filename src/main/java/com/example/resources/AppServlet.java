deff9736aa1a3917c94624aac062272193fe8c25
Relari <renzolavador@gmail.com>
2023-08-05 21:51:20 -0500

    update

---
+++ src/main/java/com/example/resources/AppServlet.java
@@ -0,0 +1,26 @@
+package com.example.resources;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet
+public class AppServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Procesar la petición GET
+		String mensaje = "Hola desde el servlet!";
+		request.setAttribute("mensaje", mensaje);
+
+		// Redireccionar a la página JSP
+		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+	}
+
+}
